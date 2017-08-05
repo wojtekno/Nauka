1514bd66cf11a0ae44af24f88aeff684786fd2fd
wojtekno <wjw.nowak@gmail.com>
2017-08-05 15:01:17 +0200

    I made the tankGas method in Car

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,16 @@
 
 public class Car {
 	//fields
-	private double maxCapacityOfTank;
-	private double fuel;
+	private double maxCapacityOfTank = 50;
+	private double fuel = 35;
 	String color;
+	private String typeOfGas = "GAZ";
 	
 	void printSpec () {
 		System.out.println("Capacity: " + maxCapacityOfTank + "\n"
 				+ "Color: " + color + "\n"
-				+ "Fuel: " + fuel);
+				+ "Fuel: " + fuel + "\n"
+				+ "Type of gas: " + typeOfGas);
 	}
 	//getter
 	double getFuel () {
@@ -16,15 +18,37 @@ public class Car {
 	}
 	//setter
 	void setMaxCappacityOfTank (double maxCapacity) {
-		if (maxCapacity < 5 || 150 < maxCapacity) {
-			System.out.println("You cannot because it has to be beetwen 5 an 150");
-		}
-		else {
+		// czy mogę tak 5 < a < 15 ??
+		if ( 5 < maxCapacity && maxCapacity < 150 ) {
 			maxCapacityOfTank = maxCapacity;
 		}
 		
 	}
-
+	
+	String getTypeOfGas () {
+		return typeOfGas;
+	}
+	
+	void tankYourCar (String typeOfGas, int quantityOfGas) {
+		if ( typeOfGas .equals(this.typeOfGas)) {
+			if ((maxCapacityOfTank - fuel) < quantityOfGas ) {
+				System.out.println("you could tank only: "+ (maxCapacityOfTank - this.fuel) + " litres of " + this.typeOfGas);
+				fuel = maxCapacityOfTank;
+				System.out.println("you did it, and now your tank is full: " +(int)fuel + " litres");
+				
+			}
+			else {
+			fuel = fuel + quantityOfGas;
+			System.out.println("You put " + quantityOfGas + " litres of gas and now there is " + (int)fuel + " litres");
+			
+			
+			}
+		}
+		else {
+			System.out.println("you want to put wrong type of gas.\n"   
+					+ "Try " + this.typeOfGas);
+		}
+	}
 }
 
 //zrob funkcje tankuj(pomysl co bedzie przyjmowac i zwracac)
@@ -33,4 +57,4 @@ public class Car {
 // naleznosc za paliwo 
 // car - benzyna/gaz/ropa
 // komunikat z odpowiednia benzyna
-// string sie nieporuwnje "==" tylko  ".="
+// string sie nieporuwnje "==" tylko  ".equals"
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
 public class Main {
-
+			// static - nie musi miec obiektu, uzywa sie do klas util
+	
 	public static void main(String[] args) {
-
+		//literał
 		 int someNumber = 8;
+		//Typ silny, 
+		// Integer 
 		// Methods m = new Methods();
 		// m.firstMethod();
 		// m.firstMethod();
@@ -19,20 +22,26 @@ public class Main {
 		// System.out.println(m.mean(3, 2, 6));
 		// System.out.println(m.charToInt('a'));
 		// m.fooBar();
-		//
+//		//
 		Car maluch = new Car();
-		maluch.printSpec();
-		maluch.color = "yellow";
-		maluch.printSpec();
-		System.out.println(maluch.color);
-		//po kropce jest czyms innym
-		maluch.color.concat("asd").contains(maluch.color);
-		System.out.println(maluch);
-		//nie mozna bo private
-//		maluch.fuel = 100;
+//		maluch.printSpec();
+//		maluch.color = "yellow";
+//		maluch.printSpec();
+//		System.out.println(maluch.color);
+//		//po kropce jest czyms innym
+//		maluch.color.concat("asd").contains(maluch.color);
+//		System.out.println(maluch);
+//		//nie mozna bo private
+////		maluch.fuel = 100;
+		System.out.println(maluch.getTypeOfGas());
 		System.out.println(maluch.getFuel());
-		maluch.setMaxCappacityOfTank(155);
+//		maluch.setMaxCappacityOfTank(45);
 		maluch.printSpec();
+		
+		maluch.tankYourCar("GAZ", 7);
+		
+		
+		
 	}
 
 }
